993f316ba08761c44f86573cb069ac112bdd19ca
Arpita Neware <99759509+arpitaneware06@users.noreply.github.com>
2024-01-20 18:58:18 +0530

    JS

---
+++ math.js
@@ -0,0 +1,14 @@
+/*const sum=(a,b)=>a+b;
+const mul=(a,b)=>(a*b);
+const pi=3.14;
+const g=1.4;
+
+
+ exports.sum =(a,b)=>a+b;*/
+ const info=require("fruits");
+
+ console.log(info);
+
+ 
+
+   
---
+++ script.js
@@ -0,0 +1,16 @@
+/*let n=5;
+for(let i=0;i<n;i++){
+    console.log("Hello ", i);
+}
+console.log("bye>>>");
+
+let args=process.argv;
+for(let i=2;i<args.length;i++){
+    console.log("Hello to",args[i]);
+}
+const info=require("./fruits");
+
+console.log(info);*/
+
+import { generate,} from "random-words";
+console.log(generate());
